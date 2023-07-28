fddebab51e35bbccdc8514fdded3064129340518
Rohan Parkes <rparkes@email.com>
2023-07-28 12:53:40 +1000

    Replaced nginx as reverse proxy

--- Program.cs
+++ Program.cs
@@ -1,52 +1,5 @@
 using ExampleApp;
 using ExampleApp.Models;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-
-/*
-var builder = WebApplication.CreateBuilder(args);
-
-var configuration = builder.Configuration;
-
-var host = configuration["DBHOST"] ?? "localhost";
-var port = configuration["DBPORT"] ?? "3306";
-var password = configuration["DBPASSWORD"] ?? "mysecret";
-
-var connectionString = $"server={host};port={port};database=products;user=root;password={password};";
-
-builder.Services.AddTransient<IRepository, DummyRepository>();
-builder.Services.AddDbContext<ProductDbContext>(options =>
-    options.UseMySQL(connectionString)
-);
-
-builder.Services.AddSingleton<IConfiguration>(configuration);
-builder.Services.AddTransient<IRepository, ProductRepository>();
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseStaticFiles();
-app.UseDeveloperExceptionPage();
-app.UseStatusCodePages();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-*/
 
 var config = new ConfigurationBuilder()
     .AddCommandLine(args)
@@ -66,7 +19,6 @@ else
     .UseConfiguration(config)
     .UseKestrel()
     .UseContentRoot(Directory.GetCurrentDirectory())
-    .UseIISIntegration()
     .UseStartup<Startup>()
     .Build();
     
